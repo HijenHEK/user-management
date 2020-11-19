988985709ab4fa3ff26fc7252697f33f6abe112d
HijenHek <k.hijen@gmail.com>
2020-11-19 20:29:05 +0100

    auth bootstrap ui

--- app/Providers/FortifyServiceProvider.php
+++ app/Providers/FortifyServiceProvider.php
@@ -6,8 +6,10 @@ use App\Actions\Fortify\CreateNewUser;
 use App\Actions\Fortify\ResetUserPassword;
 use App\Actions\Fortify\UpdateUserPassword;
 use App\Actions\Fortify\UpdateUserProfileInformation;
+use Illuminate\Database\Console\Migrations\InstallCommand;
 use Illuminate\Support\ServiceProvider;
 use Laravel\Fortify\Fortify;
+
 
 class FortifyServiceProvider extends ServiceProvider
 {
@@ -32,5 +34,34 @@ class FortifyServiceProvider extends ServiceProvider
         Fortify::updateUserProfileInformationUsing(UpdateUserProfileInformation::class);
         Fortify::updateUserPasswordsUsing(UpdateUserPassword::class);
         Fortify::resetUserPasswordsUsing(ResetUserPassword::class);
+
+        
+            
+                Fortify::loginView(function () {
+                    return view('auth.login');
+                });
+    
+                Fortify::registerView(function () {
+                    return view('auth.register');
+                });
+    
+                Fortify::confirmPasswordView(function () {
+                    return view('auth.passwords.confirm');
+                });
+    
+                Fortify::requestPasswordResetLinkView(function () {
+                    return view('auth.passwords.email');
+                });
+    
+                Fortify::resetPasswordView(function () {
+                    return view('auth.passwords.reset');
+                });
+    
+                Fortify::verifyEmailView(function () {
+                    return view('auth.verify');
+                });
+    
+            }
+        
     }
-}
+
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,13 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::middleware(['auth'])->group(function() {
+    Route::get('/home', function() {
+        return view('home');
+    })->name('home');
+
+    Route::get('/user/profile', function() {
+        return view('profile');
+    })->name('profile');
+});
